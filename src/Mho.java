21fe802e954c24bf5a5a894829482ef307490560
as-f <as-f@users.noreply.github.com>
2015-09-15 11:30:24 -0700

    Create Mho.java

---
+++ src/Mho.java
@@ -0,0 +1,20 @@
+// classed made by Albert
+
+public class Mho {
+
+	private int x;
+	private int y;
+	
+	public Mho(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/*
+	 * 
+	 */
+	public void draw() {
+		
+	}
+	
+}
