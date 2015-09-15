0114e87f17d54cdde415825aef17f1420d4dc833
tinysqueak <tinysqueak@gmail.com>
2015-09-15 09:45:41 -0700

    Update Fence.java
    Trying to see if I can get the fence set to an image.

--- src/Fence.java
+++ src/Fence.java
@@ -1,6 +1,27 @@
 
+import java.awt.*;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Fence {
+	
+	private Image fence;;
+	
+	public Fence() {
+		
+		try {
+			
+			fence = ImageIO.read(new File("fence.jpg"));
+			
+		} catch (IOException e) {
+			
+			
+		}
+		
+	}
 	
 	
 
 }
+
