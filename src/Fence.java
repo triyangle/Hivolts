59b36e183419e1d19b936ac4ffb775328f972ac4
tinysqueak <tinysqueak@gmail.com>
2015-09-17 09:48:23 -0700

    Made a fence image
    The fence is too big right now, someone fix pls

--- src/Fence.java
+++ src/Fence.java
@@ -1,27 +1,39 @@
 
 import java.awt.*;
-import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 import javax.imageio.ImageIO;
 
 public class Fence {
-	
-	private Image fence;;
-	
-	public Fence() {
+
+	private BufferedImage fence;
+	private Graphics g;
+
+	public Fence(Graphics g) {
+		
+		this.g = g;
+		//drawFence(g);
+
+	}
+
+	//works, but fence is too big
+	public void drawFence() {
 		
 		try {
-			
-			fence = ImageIO.read(new File("fence.jpg"));
-			
+
+			fence = ImageIO.read(new File("fence.png"));
+
 		} catch (IOException e) {
-			
-			
+
+
+
 		}
 		
+		//use other drawImage method in Graphics class to scale height/width
+		g.drawImage(fence, 0, 0, null);
+
 	}
-	
-	
 
 }
 
