2e1de13f849cb19b3c8b77d0e02add47eda92986
as-f <as-f@users.noreply.github.com>
2015-09-17 11:31:30 -0700

    Create Entity.java

---
+++ src/Entity.java
@@ -0,0 +1,32 @@
+
+public class Entity {
+
+	protected int x;
+	protected int y;
+	
+	public Entity() {
+		
+		
+	}
+	
+	public void act() {
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
