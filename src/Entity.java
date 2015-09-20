020387acc7c580eacf694c194c2437db3447c33c
as-f <as-f@users.noreply.github.com>
2015-09-19 17:53:18 -0700

    Update Entity.java to handle colors and drawing

--- src/Entity.java
+++ src/Entity.java
@@ -1,8 +1,13 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Entity {
 
 	protected int x;
 	protected int y;
+	
+	protected Color myColor;
 	
 	public Entity() {
 		
@@ -27,6 +32,21 @@ public class Entity {
 	
 	public void setY(int y) {
 		this.y = y;
+	}
+	
+	/**
+	 * Draws an entity with a certain color
+	 * @param xOffset The horizontal distance before the first cell
+	 * @param yOffset The vertical distance before the first cell
+	 * @param width The width of a cell
+	 * @param height The height of a cell
+	 * @param g The graphics object
+	 */
+	public void draw(int xOffset, int yOffset, int width, int height, Graphics g) {
+		int xLeft = xOffset + 1 + (this.x * (width + 1));
+		int yTop = yOffset + 1 + (this.y * (height + 1));
+		g.setColor(myColor);
+		g.fillRect(xLeft, yTop, width, height);
 	}
 	
 }
