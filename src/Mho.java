4c5d97a2931ec1514be6823c14d08484bb0394cf
as-f <as-f@users.noreply.github.com>
2015-09-20 19:20:26 -0700

    Clean up Mho.java

--- src/Mho.java
+++ src/Mho.java
@@ -4,9 +4,7 @@ import java.awt.Color;
 /**
  * 
  * @author Albert
- *
  */
-// class made by Albert
 
 public class Mho extends Entity {
 
@@ -37,25 +35,6 @@ public class Mho extends Entity {
 		return alive;
 		
 	}
-	
-	//places one particular mho
-	// Mhos should not be a property of a cell because that makes it awkward to move them around
-	/*public void placeMho(Cell[][] cell) {
-		
-		x = (int)(Math.random() * 12);
-		y = (int)(Math.random() * 12);
-		
-		//assigns new coordinates until cell is not occupied
-		while(cell[x][y].getOccupied() == true) {
-			
-			x = (int)(Math.random() * 12);
-			y = (int)(Math.random() * 12);
-			
-		}
-		
-		cell[x][y].setMho();
-		
-	}*/
 	
 	public void nextTurn() {
 		
